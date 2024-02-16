b362cdb5853a6f5de053768f9cc24b0f65480494
Ye Htun <156397048+yehtun-MIT@users.noreply.github.com>
2024-02-16 11:54:53 +0630

    datatable style change

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -34,6 +34,6 @@ class Role extends Model
     
     public function permissions()
     {
-        return $this->belongsToMany(Permission::class);
+        return $this->belongsToMany(Permission::class,'permission_role','role_id','permission_id');
     }
 }
